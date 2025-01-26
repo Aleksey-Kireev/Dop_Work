fbba059921d4e4618b2b43f348915fd1dfd5129e
Aleksey-Kireev <kireev_a_a@mail.ru>
2025-01-27 02:53:21 +0900

    В этом репозитории работы по дополнительным заданиям.

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+
+//   Дополнительные задания
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Дополнительные задания.");
+        }
+    }
