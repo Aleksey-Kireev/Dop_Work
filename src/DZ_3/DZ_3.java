347d89068d3f25b1db310d72d743430c41428753
Aleksey-Kireev <kireev_a_a@mail.ru>
2025-02-16 12:47:40 +0900

    ------  Доп. задания  ----- Добавил новый пакет и класс.

---
+++ src/DZ_3/DZ_3.java
@@ -0,0 +1,15 @@
+package DZ_3;
+
+public class DZ_3 {
+    public static void genArray_DZ_3() {
+        System.out.println("Работа с новым пакетом и классом ");
+
+
+
+    }
+
+
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import DZ_3.DZ_3;
+
 import java.util.Scanner;
 import java.util.Arrays;
 
@@ -80,7 +82,7 @@ public class Main {
         //   ---- Способ 1, через for
 
         System.out.println(" \n ---- Вывод способом № 1 ----");
-        for ( int i = 0; i < bSize; i++){
+        for (int i = 0; i < bSize; i++) {
             for (int j = 0; j < 10; j++) {
                 System.out.print(array[i][j] + " ");
             }
@@ -95,6 +97,89 @@ public class Main {
             System.out.println();
         }
 
+//        Задание № 3   Преобразование числа в строку с указанной системой исчисления
+
+        int number = 72; // Исходное целое число
+        int radix = 16; // Основание системы счисления (например, 16 для шестнадцатеричной)
+
+        // Преобразование числа в строку в указанной системе счисления
+        String numberString = Integer.toString(number, radix);
+
+        System.out.println(" \n Число " + number + " в системе счисления с основанием " + radix + " равно: " + numberString);
+
+//          Задание № 4   Создайте массив из 11 случайных чисел из промежутка [start;end].
+//          Выведите данный массив в консоль. Напишите программу, которая определяет,
+//          какой элемент встречается в массиве чаще всего. Выведите данную информацию
+//          в консоль. Если два разных элемента встречаются одинаковое количество раз,
+//          то выведите -1
+
+        System.out.println();
+        for (int i = 0; i < 5; i++) {           //      Задаем количество повторений кода
+            task4();                            //      Вызываем метод выполнения задания 4
+        }
+
+        DZ_3.genArray_DZ_3();
+
 
     }
+
+    public static void task4() {
+        int start = 1;                       //      Задаем нижнюю границу
+        int end = 4;                        //      Задаем верхнюю границу
+        int[] nums = generateRandomArray(start, end, 11);   //      Вызываем метод заполнения массива в указанных
+        // границах
+        System.out.println(Arrays.toString(nums));              //      выводим получившийся массив
+
+//                Цикл подсчета количества одинаковых элементов в массиве
+        int[] numCount = new int[end - start];
+        for (int num : nums) {
+            int index = num - start;
+            numCount[index]++;
+        }
+        System.out.println();
+        System.out.print(Arrays.toString(numCount));            //      Вывод количества в консоль
+        System.out.println();
+
+//                Определение популярного и второго по популярности элемента
+        int mostPopularNum = 0;
+        int secondMostPopularNum = 0;
+        int max = numCount[0];
+        for (int i = 0; i < numCount.length; i++) {
+            int count = numCount[i];
+
+            if (count >= max) {
+                secondMostPopularNum = mostPopularNum;
+                mostPopularNum = i + start;
+                max = count;
+                continue;
+            }
+
+            if (count > secondMostPopularNum) {
+                secondMostPopularNum = i + start;
+            }
+        }
+
+        int result = mostPopularNum;
+        if (mostPopularNum == secondMostPopularNum) {
+            result = -1;
+        }
+        System.out.println(result);
+    }
+
+    private static int[] generateRandomArray(int start, int end, int size) {
+        if (start > end || size < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        int[] arr = new int[size];
+        int diff = end - start;
+
+        for (int i = 0; i < size; i++) {
+            float randomDouble = (float) (Math.random() * diff + start);
+            arr[i] = (int) randomDouble;
+        }
+
+        return arr;
+    }
+
 }
